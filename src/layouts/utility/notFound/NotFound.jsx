fcd7c7b7696aa5a152faadddef1115fd0f14c3fc
fahdpixel <fahd.pixel@gmail.com>
2023-02-03 18:57:31 +0530

    FEAT: NotFound Page and API integration

---
+++ src/layouts/utility/notFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from "../../../assets/images/18-dark.png"
+const NotFound = () => {
+  return (
+    <div className='flex-center m-5'>
+        <img src={Image} alt="" className='img-fluid w-20-rem h-22-rem'/>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/views/app/pages/notFound/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../../../layouts/utility/footer/Footer";
+import Header from "../../../../layouts/utility/header/Header";
+import NotFound from "../../../../layouts/utility/notFound/NotFound";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <Header />
+      <NotFound />
+      <Footer />
+    </>
+  );
+};
+
+export default NotFoundPage;
