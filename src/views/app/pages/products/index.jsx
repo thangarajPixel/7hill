a0884dbb7ab53029b31e1eff9422c3d6a1e090c8
Fahd <fahd.pixel@gmail.com>
2023-02-07 11:31:39 +0530

    FEAT: Product API integration

--- src/views/app/pages/products/index.jsx
+++ src/views/app/pages/products/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../../../../layouts/utility/header/Header";
 import Footer from "../../../../layouts/utility/footer/Footer";
 import ProductListingBanner from "../../../../assets/images/product-listing-banner.jpg";
@@ -7,8 +7,34 @@ import ProductListing from "../../components/productListing";
 import OtherProductsScroll from "../../components/otherProductScroll";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Helmet } from "react-helmet";
+import axios from "axios";
 
 const Products = () => {
+  useEffect(() => {
+    getProducts();
+    // eslint-disable-next-line
+  }, []);
+
+  var formdata = new FormData();
+  formdata.append("category ", "Bed");
+
+  var requestOptions = {
+    method: "GET",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  const getProducts = () => {
+    return axios
+      .get(
+        "http://192.168.0.56/7hillAdmin/public/api/get/products",
+        requestOptions
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <>
       <Helmet>
