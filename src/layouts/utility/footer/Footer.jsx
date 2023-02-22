b109005638d481198d98ed4a3e3b028b70f5aafb
Fahd <fahd.pixel@gmail.com>
2023-02-22 16:54:28 +0530

    Fix : Footer

--- src/layouts/utility/footer/Footer.jsx
+++ src/layouts/utility/footer/Footer.jsx
@@ -9,9 +9,9 @@ import { FiPhone } from "react-icons/fi";
 import { RiMapPinLine } from "react-icons/ri";
 import { TfiEmail } from "react-icons/tfi";
 import { AiOutlineInstagram } from "react-icons/ai";
-import { RiLinkedinFill, RiFacebookFill, RiTwitterFill } from 'react-icons/ri';
+import { RiLinkedinFill, RiFacebookFill, RiTwitterFill } from "react-icons/ri";
 import ListGroup from "react-bootstrap/ListGroup";
-import WhatsappIcon from '../../../assets/images/whatsapp-icon.png';
+import WhatsappIcon from "../../../assets/images/whatsapp-icon.png";
 import { useSelector } from "react-redux";
 
 const openInNewTab = (url) => {
@@ -23,7 +23,6 @@ const getCurrentYear = () => {
 };
 
 const Footer = () => {
-
   const location = useLocation();
   const { pathname } = location;
   const splitLocation = pathname.split("/");
@@ -32,7 +31,8 @@ const Footer = () => {
   const [homeFurniture, setHomeFurniture] = useState("");
   const [institutionalFurniture, setInstitutionalFurniture] = useState("");
   useEffect(() => {
-    let homeFurn = category && category?.filter((item) => item.slug === "home-furniture");
+    let homeFurn =
+      category && category?.filter((item) => item.slug === "home-furniture");
     let institutionalFurn = category?.filter(
       (item) => item.slug === "institutional-furniture"
     );
@@ -86,37 +86,95 @@ const Footer = () => {
             </p>
             <ListGroup>
               <ListGroup.Item>
-                <Link rel="noopener noreferrer" onClick={() => openInNewTab('https://www.instagram.com/07hillfurniture/')} >
+                <Link
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    openInNewTab("https://www.instagram.com/07hillfurniture/")
+                  }
+                >
                   <AiOutlineInstagram />
                 </Link>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Link rel="noopener noreferrer" onClick={() => openInNewTab('https://www.linkedin.com/company/7hillfurniture/')} >
+                <Link
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    openInNewTab(
+                      "https://www.linkedin.com/company/7hillfurniture/"
+                    )
+                  }
+                >
                   <RiLinkedinFill />
                 </Link>
               </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link rel="noopener noreferrer" onClick={() => openInNewTab('https://twitter.com/7hillfurniture/')} >
-                    <RiTwitterFill />
-                  </Link>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Link rel="noopener noreferrer" onClick={() => openInNewTab('https://www.facebook.com/7hillfurniture/')} >
-                    <RiFacebookFill />
-                  </Link>
-                </ListGroup.Item>    
+              <ListGroup.Item>
+                <Link
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    openInNewTab("https://twitter.com/7hillfurniture/")
+                  }
+                >
+                  <RiTwitterFill />
+                </Link>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Link
+                  rel="noopener noreferrer"
+                  onClick={() =>
+                    openInNewTab("https://www.facebook.com/7hillfurniture/")
+                  }
+                >
+                  <RiFacebookFill />
+                </Link>
+              </ListGroup.Item>
             </ListGroup>
           </Col>
           <Col xs={12} sm={12} md={6} lg={2} xl={2}>
             <h5>Quick Links</h5>
             <div className="footer-links">
-              <Link className={splitLocation[1] === "" ? "active" : ""} to="/" >Home</Link>
-              <Link className={splitLocation[1] === "home-furniture" ? "active" : ""} to="/home-furniture" >Products</Link>
-              <Link className={splitLocation[1] === "about" ? "active" : ""} to="/about" >About Us</Link>
-              <Link className={splitLocation[1] === "dealers" ? "active" : ""} to="/dealers" >Dealers</Link>
-              <Link className={splitLocation[1] === "infrastructure" ? "active" : ""} to="/infrastructure" >Infrastructure</Link>
-              <Link className={splitLocation[1] === "careers" ? "active" : ""} to="/careers" >Careers</Link>
-              <Link className={splitLocation[1] === "contact" ? "active" : ""} to="/contact" >Contact Us</Link>
+              <Link className={splitLocation[1] === "" ? "active" : ""} to="/">
+                Home
+              </Link>
+              <Link
+                className={
+                  splitLocation[1] === "home-furniture" ? "active" : ""
+                }
+                to="/home-furniture"
+              >
+                Products
+              </Link>
+              <Link
+                className={splitLocation[1] === "about" ? "active" : ""}
+                to="/about"
+              >
+                About Us
+              </Link>
+              <Link
+                className={splitLocation[1] === "dealers" ? "active" : ""}
+                to="/dealers"
+              >
+                Dealers
+              </Link>
+              <Link
+                className={
+                  splitLocation[1] === "infrastructure" ? "active" : ""
+                }
+                to="/infrastructure"
+              >
+                Infrastructure
+              </Link>
+              <Link
+                className={splitLocation[1] === "careers" ? "active" : ""}
+                to="/careers"
+              >
+                Careers
+              </Link>
+              <Link
+                className={splitLocation[1] === "contact" ? "active" : ""}
+                to="/contact"
+              >
+                Contact Us
+              </Link>
             </div>
           </Col>
           <Col xs={12} sm={12} md={6} lg={4} xl={4}>
@@ -137,16 +195,16 @@ const Footer = () => {
             <h5>Institutional Furniture</h5>
             <div className="footer-links">
               {institutionalFurniture &&
-                institutionalFurniture.map((item, i) => {
-                  return item.child?.map((item, i) => {
-                    return(
+                institutionalFurniture.map((item) => {
+                  return item.subChild?.map((item, i) => {
+                    return (
                       <Link
                         to={`/institutional-furniture/${item.slug}`}
                         key={i}
                       >
                         {item.name}
                       </Link>
-                    )
+                    );
                   });
                 })}
             </div>
@@ -155,21 +213,42 @@ const Footer = () => {
         <Row className="footer-bottom">
           <Col xs={12} sm={12} md={12} lg={6} xl={6}>
             <p className="footer-bottom-text1">
-              Copyright &copy; {getCurrentYear()} 7Hill Furniture. A unit of Hevea Furniture & Interior Pvt Ltd | All Rights
-              Reserved.</p>
+              Copyright &copy; {getCurrentYear()} 7Hill Furniture. A unit of
+              Hevea Furniture & Interior Pvt Ltd | All Rights Reserved.
+            </p>
           </Col>
           <Col xs={12} sm={12} md={12} lg={6} xl={6}>
             <p className="footer-bottom-text2">
-            <Link className={splitLocation[1] === "privacy-policy" ? "active" : ""} to="/privacy-policy" >Privacy Policy</Link>
-                  <Link className={splitLocation[1] === "discrimination-policy" ? "active" : ""} to="/discrimination-policy" >Discrimination Policy</Link>
+              <Link
+                className={
+                  splitLocation[1] === "privacy-policy" ? "active" : ""
+                }
+                to="/privacy-policy"
+              >
+                Privacy Policy
+              </Link>
+              <Link
+                className={
+                  splitLocation[1] === "discrimination-policy" ? "active" : ""
+                }
+                to="/discrimination-policy"
+              >
+                Discrimination Policy
+              </Link>
             </p>
           </Col>
         </Row>
       </Container>
       {/* eslint-disable-next-line */}
-      <Link rel="noopener noreferrer" className="whatsapp-btn" onClick={() => openInNewTab('https://api.whatsapp.com/send?phone=918881381388')} >
-          <img src={WhatsappIcon} alt="" />
-        </Link>
+      <Link
+        rel="noopener noreferrer"
+        className="whatsapp-btn"
+        onClick={() =>
+          openInNewTab("https://api.whatsapp.com/send?phone=918881381388")
+        }
+      >
+        <img src={WhatsappIcon} alt="" />
+      </Link>
     </footer>
   );
 };
