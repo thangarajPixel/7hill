f8dea9542e0bf715f4d3b5e89ef7fc1bb475d02b
thangarajPixel <thangaraj.pixel@gmail.com>
2023-03-29 14:34:27 +0530

    privacy-policy-changes

--- src/views/app/components/privacyPolicyContent/index.jsx
+++ src/views/app/components/privacyPolicyContent/index.jsx
@@ -15,9 +15,9 @@ const PrivacyPolicyContent = () => {
             <Row>
             <Col>
               <h2 className="heading2 mar-bot-20">PRIVACY POLICY</h2>
-              <p className=" mar-bot-20">At Hevea products, accessible from www.heveaproducts.co.in, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hevea products and how we use it.</p>
+              <p className=" mar-bot-20">At 7Hill Furniture, accessible from https://7hillfurniture.com/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by 7Hill Furniture products and how we use it.</p>
               <p className=" mar-bot-20">If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
-              <p className=" mar-bot-20">This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Hevea products. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+              <p className=" mar-bot-20">This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in 7Hill Furniture products. This policy is not applicable to any information collected offline or via channels other than this website.</p>
               <h5 className="heading5 pt-4">CONSENT</h5>
               <p className=" mar-bot-20">By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
               <h5 className="heading5 pt-4">INFORMATION WE COLLECT</h5>
@@ -36,13 +36,13 @@ const PrivacyPolicyContent = () => {
                 <ListGroup.Item><BsCheck2Circle /> Find and prevent fraud</ListGroup.Item>
               </ListGroup>  
               <h5 className="heading5 pt-4">LOG FILES</h5>
-              <p className=" mar-bot-20">Hevea products follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+              <p className=" mar-bot-20">7Hill Furniture products follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
               <h5 className="heading5 pt-4">ADVERTISING PARTNERS PRIVACY POLICIES</h5>
-              <p className=" mar-bot-20">You may consult this list to find the Privacy Policy for each of the advertising partners of Hevea products.</p>
-              <p className=" mar-bot-20">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Hevea products, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-              <p className=" mar-bot-20">Note that Hevea products has no access to or control over these cookies that are used by third-party advertisers.</p>
+              <p className=" mar-bot-20">You may consult this list to find the Privacy Policy for each of the advertising partners of 7Hill Furniture products.</p>
+              <p className=" mar-bot-20">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on 7Hill Furniture products, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+              <p className=" mar-bot-20">Note that 7Hill Furniture products has no access to or control over these cookies that are used by third-party advertisers.</p>
               <h5 className="heading5 pt-4">THIRD PARTY PRIVACY POLICIES</h5>
-              <p className=" mar-bot-20">Hevea product's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.</p>
+              <p className=" mar-bot-20">7Hill Furniture product's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.</p>
               <p className=" mar-bot-20">You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
               <h5 className="heading5 pt-4">CCPA PRIVACY RIGHTS (DO NOT SELL MY PERSONAL INFORMATION)</h5>
               <p className=" mar-bot-20">Under the CCPA, among other rights, California consumers have the right to:</p>
@@ -61,7 +61,7 @@ const PrivacyPolicyContent = () => {
               <p className=" mar-bot-20">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
               <h5 className="heading5 pt-4">CHILDREN'S INFORMATION</h5>
               <p className=" mar-bot-20">Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
-              <p className=" mar-bot-20">Hevea products does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+              <p className=" mar-bot-20">7Hill Furniture products does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
               <p className=" mar-bot-20"><img src={Signature} alt="" /><br/><strong>E.RAJENDRAN</strong><br/>Director</p>
             </Col>
             </Row>
